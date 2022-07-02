1c56e6784e739bc4a0dca90015f46293d3394c52
Mohammed Jaseem <56159302+Mohammedjaseem@users.noreply.github.com>
2022-07-02 23:12:00 +0530

    com

---
+++ Week 01/Program01.py
@@ -0,0 +1,4 @@
+# Accept a char input from the user and display it on the console.
+
+charather = input("Enter the charather: ")
+print("Entered Charater is: ",charather)
---
+++ Week 01/Program02.py
@@ -0,0 +1,6 @@
+# Accept two inputs from the user and output its sum.
+
+num1 = int(input("Enter 1st Number: "))
+num2 = float(input("Enter 2nd Number: "))
+sum = float(num1+num2)
+print ("Sum is ",sum) 
