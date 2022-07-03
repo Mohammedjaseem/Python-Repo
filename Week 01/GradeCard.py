51f8ca276ecf18ea056a5ba28784d6b23d779ef1
Mohammed Jaseem <56159302+Mohammedjaseem@users.noreply.github.com>
2022-07-03 19:54:55 +0530

    Grade Card

--- Week 01/GradeCard.py
+++ Week 01/GradeCard.py
@@ -1,4 +1,22 @@
+'''
+Grades are computed using a weighted average. Suppose that the written test counts 70%,  lab exams 20% and assignments 10%.
+If Arun has a score of
+Written test = 81
+Lab exams = 68
+Assignments = 92
+Arun’s overall grade = (81x70)/100 + (68x20)/100 + (92x10)/100 = 79.5
+ Write a program to find the grade of a student during his academic year. 
+Program should accept the scores for written test, lab exams and assignments
+Output the grade of a student (using weighted average)
+Eg:
+Enter the marks scored by the students
+Written test = 55
+Lab exams = 73
+Assignments = 87
+Grade of the student is 61.8
 
+
+'''
 
 def avg_written_test(mark):
     avg_written_test_mark=(mark*70)/100
