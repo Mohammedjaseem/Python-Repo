aaf1f40bad1424b95579fa4e62afc4fc3d29072c
Mohammed Jaseem <56159302+Mohammedjaseem@users.noreply.github.com>
2022-07-06 18:55:45 +0530

    Cordinates Point Ziping

--- Week 01/indexing .py
+++ Week 01/indexing .py
@@ -1,3 +1,10 @@
 items=['bananas','mattresses','dog kennels','machine','cheeses']
+print("\nPrint using zip function: \n")
 for i, item in zip(range(len(items)),items):
+    print(i,item)
+
+
+# or enumarate function can be used to get the index of the item
+print("\nPrint using enumarate function: \n")
+for i, item in enumerate(items):
     print(i,item)
